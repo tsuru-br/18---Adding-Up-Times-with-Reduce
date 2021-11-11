d5f74b6ace5b9b004035d435c9f3793f943ba9dd
Gabriel Tsurushima <protsuru@gmail.com>
2021-11-11 11:25:45 -0300

    finished!

--- script.js
+++ script.js
@@ -9,5 +9,11 @@ const seconds = timeNodes
     .reduce((total, vidSeconds) => total + vidSeconds);
 
 let secondsLeft = seconds;
+
 const hours = Math.floor(secondsLeft / 3600);
 secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
