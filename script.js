891ef5a6310a6b1fdbf3925a400f07c6b3e771d9
Gabriel Tsurushima <protsuru@gmail.com>
2021-11-11 11:22:46 -0300

    hours

---
+++ script.js
@@ -0,0 +1,13 @@
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return (mins * 60) + secs;
+    })
+    .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
